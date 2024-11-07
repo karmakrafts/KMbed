7028f22843f67abfa63fc95d500bc65229547d20
KitsuneAlex <alexh@karmakrafts.dev>
2024-11-07 13:44:38 +0100

    Strip all previous file headers/comments which contained mismatched copyright information

--- kmbed-compiler-plugin/src/main/kotlin/net/folivo/kmbed/compiler/KMbedIrGenerationExtension.kt
+++ kmbed-compiler-plugin/src/main/kotlin/net/folivo/kmbed/compiler/KMbedIrGenerationExtension.kt
@@ -4,10 +4,6 @@ import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 
-/**
- * @author Alexander Hinze
- * @since 29/10/2024
- */
 class KMbedIrGenerationExtension : IrGenerationExtension {
     override fun generate(moduleFragment: IrModuleFragment, pluginContext: IrPluginContext) {
 
