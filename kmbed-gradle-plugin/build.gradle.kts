9de1ba26d000b34885802bf17e75af2554565a86
KitsuneAlex <alexh@karmakrafts.dev>
2025-01-20 23:22:47 +0100

    Add missing publishing configurations to submodules

--- kmbed-gradle-plugin/build.gradle.kts
+++ kmbed-gradle-plugin/build.gradle.kts
@@ -39,7 +39,8 @@ tasks {
     compileKotlin { dependsOn(processResources) }
 }
 
-@Suppress("UnstableApiUsage") gradlePlugin {
+@Suppress("UnstableApiUsage")
+gradlePlugin {
     System.getenv("CI_PROJECT_URL")?.let {
         website.set(it)
         vcsUrl.set(it)
@@ -51,6 +52,49 @@ tasks {
             displayName = "KMbed Gradle Plugin"
             description = "Gradle plugin for applying the KMbed Kotlin compiler plugin"
             tags.addAll("kotlin", "native", "interop", "codegen")
+        }
+    }
+}
+
+publishing {
+    System.getenv("CI_API_V4_URL")?.let { apiUrl ->
+        repositories {
+            maven {
+                url = uri("$apiUrl/projects/${System.getenv("CI_PROJECT_ID")}/packages/maven")
+                name = "GitLab"
+                credentials(HttpHeaderCredentials::class) {
+                    name = "Job-Token"
+                    value = System.getenv("CI_JOB_TOKEN")
+                }
+                authentication {
+                    create("header", HttpHeaderAuthentication::class)
+                }
+            }
+        }
+    }
+    publications.configureEach {
+        if (this is MavenPublication) {
+            pom {
+                name = project.name
+                description = "Embedded resource tooling for Kotlin/Native."
+                url = System.getenv("CI_PROJECT_URL")
+                licenses {
+                    license {
+                        name = "Apache License 2.0"
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "kitsunealex"
+                        name = "KitsuneAlex"
+                        url = "https://git.karmakrafts.dev/KitsuneAlex"
+                    }
+                }
+                scm {
+                    url = this@pom.url
+                }
+            }
         }
     }
 }
--- kmbed-runtime/build.gradle.kts
+++ kmbed-runtime/build.gradle.kts
@@ -26,3 +26,46 @@ kotlin {
         }
     }
 }
+
+publishing {
+    System.getenv("CI_API_V4_URL")?.let { apiUrl ->
+        repositories {
+            maven {
+                url = uri("$apiUrl/projects/${System.getenv("CI_PROJECT_ID")}/packages/maven")
+                name = "GitLab"
+                credentials(HttpHeaderCredentials::class) {
+                    name = "Job-Token"
+                    value = System.getenv("CI_JOB_TOKEN")
+                }
+                authentication {
+                    create("header", HttpHeaderAuthentication::class)
+                }
+            }
+        }
+    }
+    publications.configureEach {
+        if (this is MavenPublication) {
+            pom {
+                name = project.name
+                description = "Embedded resource runtime for Kotlin/Native."
+                url = System.getenv("CI_PROJECT_URL")
+                licenses {
+                    license {
+                        name = "Apache License 2.0"
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "kitsunealex"
+                        name = "KitsuneAlex"
+                        url = "https://git.karmakrafts.dev/KitsuneAlex"
+                    }
+                }
+                scm {
+                    url = this@pom.url
+                }
+            }
+        }
+    }
+}
